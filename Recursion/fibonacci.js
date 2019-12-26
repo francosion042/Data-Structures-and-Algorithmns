5b61605c8a7fdb596d9bb1ae88ec4db0ee81fb7f
Anthony Nwobodo <anthonynwobodo7@gmail.com>
2019-12-26 14:37:07 +0100

    Recursion -02

--- Recursion/fibonacci.js
+++ Recursion/fibonacci.js
@@ -1,14 +1,27 @@
-const fibo = n => {
-  if (n <= 1) return n;
-  var sum = 0,
-    last = 1,
-    lastLast = 0;
-  for (i = 1; i < n; i++) {
-    sum = lastLast + last;
-    lastLast = last;
-    last = sum;
+// const fibo = n => {
+//   if (n <= 1) return n;
+//   var sum = 0,
+//     last = 1,
+//     lastLast = 0;
+//   for (i = 1; i < n; i++) {
+//     sum = lastLast + last;
+//     lastLast = last;
+//     last = sum;
+//   }
+//   return sum;
+// };
+
+// console.log(fibo(10));
+
+/////////////////////////////////////////////////////////////////////////////////////
+/*with recursion*/
+
+const fiboRec = n => {
+  if (n <= 1) {
+    return n;
+  } else {
+    return fiboRec(n - 1) + fiboRec(n - 2);
   }
-  return sum;
 };
 
-console.log(fibo(10));
+console.log(fiboRec(10));
